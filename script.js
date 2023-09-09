48717e7862239cc7bdd62df16afc38e154485ea2
DIGONTA HALDER <digantahalder14000@gmail.com>
2023-09-09 09:33:24 +0530

    updated commit

--- script.js
+++ script.js
@@ -1,88 +1,91 @@
-// Function to add a new product
-function addProduct() {
-    var sellingPrice = document.getElementById("sellingPrice").value;
-    var productName = document.getElementById("productName").value;
-  
-    // Create a new product object
-    var product = {
-      sellingPrice: sellingPrice,
-      productName: productName
-    };
-  
-    // Send the new product to the server
-    $.ajax({
-      url: "https://crudcrud.com/api/f4aaa73b861842879ad5d520517d03aa/products",
-      type: "POST",
-      data: product,
-      success: function(data) {
-        // Add the new product to the list
-        addProductToList(data);
-  
+
+      // Function to add a product
+      function addProduct() {
+        // Get the values from the input fields
+        var sellingPrice = document.getElementById("sellingPrice").value;
+        var productName = document.getElementById("productName").value;
+
+        // Create a new product object
+        var product = {
+          sellingPrice: sellingPrice,
+          productName: productName
+        };
+
+        // Store the product in localStorage
+        var products = JSON.parse(localStorage.getItem("products")) || [];
+        products.push(product);
+        localStorage.setItem("products", JSON.stringify(products));
+
+        // Send the product data to the REST API
+        fetch("https://crudcrud.com/api/f4aaa73b861842879ad5d520517d03aa", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(product)
+        })
+          .then(response => response.json())
+          .then(data => console.log(data))
+          .catch(error => console.log(error));
+
         // Clear the input fields
         document.getElementById("sellingPrice").value = "";
         document.getElementById("productName").value = "";
-  
-        // Update the total value
-        updateTotalValue();
+
+        // Update the product list
+        updateProductList();
       }
-    });
-  }
-  
-  // Function to add a product to the list
-  function addProductToList(product) {
-    var productList = document.getElementById("productList");
-  
-    // Create a new list item
-    var listItem = document.createElement("li");
-    listItem.innerHTML = product.productName + " - $" + product.sellingPrice;
-  
-    // Create a delete button for the list item
-    var deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Delete";
-    deleteButton.onclick = function() {
-      deleteProduct(product._id);
-    };
-  
-    // Append the delete button to the list item
-    listItem.appendChild(deleteButton);
-  
-    // Append the list item to the product list
-    productList.appendChild(listItem);
-  }
-  
-  // Function to delete a product
-  function deleteProduct(productId) {
-    // Send a DELETE request to the server
-    $.ajax({
-      url: "https://crudcrud.com/api/f4aaa73b861842879ad5d520517d03aa/products/" + productId,
-      type: "DELETE",
-      success: function() {
-        // Remove the product from the list
-        $("#" + productId).remove();
-  
-        // Update the total value
-        updateTotalValue();
+
+      // Function to delete a product
+      function deleteProduct(index) {
+        // Retrieve the products from localStorage
+        var products = JSON.parse(localStorage.getItem("products")) || [];
+
+        // Remove the product at the specified index
+        products.splice(index, 1);
+
+        // Update the products in localStorage
+        localStorage.setItem("products", JSON.stringify(products));
+
+        // Update the product list
+        updateProductList();
       }
-    });
-  }
-  
-  // Function to update the total value
-  function updateTotalValue() {
-    // Fetch all products from the server
-    $.ajax({
-      url: "https://crudcrud.com/api/f4aaa73b861842879ad5d520517d03aa/products",
-      type: "GET",
-      success: function(data) {
+
+      // Function to update the product list
+      function updateProductList() {
+        // Retrieve the products from localStorage
+        var products = JSON.parse(localStorage.getItem("products")) || [];
+
+        // Clear the product list
+        var productList = document.getElementById("productList");
+        productList.innerHTML = "";
+
+        // Update the total value
         var totalValue = 0;
-  
-        // Calculate the total value
-        data.forEach(function(product) {
+
+        // Iterate over the products and create list items
+        products.forEach(function(product, index) {
+          // Create a list item
+          var listItem = document.createElement("li");
+          listItem.className = "list-group-item";
+          listItem.innerHTML =
+            product.sellingPrice +
+            " - " +
+            product.productName +
+            ' <button onclick="deleteProduct(' +
+            index +
+            ')" class="btn btn-danger btn-sm">Delete Product</button>';
+
+          // Add the list item to the product list
+          productList.appendChild(listItem);
+
+          // Update the total value
           totalValue += parseInt(product.sellingPrice);
         });
-  
+
         // Update the total value on the page
-        document.getElementById("totalValue").innerHTML = totalValue;
+        document.getElementById("totalValue").textContent = totalValue;
       }
-    });
-  }
-  
+
+      // Load the product list on page load
+      updateProductList();
