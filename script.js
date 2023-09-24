68430e0b409fb61d732987324ab645dac058d5de
DIGONTA HALDER <digantahalder14000@gmail.com>
2023-09-24 21:30:09 +0530

    Comment Add

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 // Function to add a product
 function addProduct() {
   // Get the values from the input fields
-  var sellingPrice = document.getElementById("sellingPrice").value;
-  var productName = document.getElementById("productName").value;
+  var sellingPrice = document.getElementById("sellingPrice").value; // Get the selling price from the input field
+  var productName = document.getElementById("productName").value; // Get the product name from the input field
 
   // Create a new product object
   var product = {
-    sellingPrice: sellingPrice,
-    productName: productName
+    sellingPrice: sellingPrice, // Assign selling price to the product object
+    productName: productName, // Assign product name to the product object
   };
 
   // Store the product in localStorage
-  var products = JSON.parse(localStorage.getItem("products")) || [];
-  products.push(product);
-  localStorage.setItem("products", JSON.stringify(products));
+  var products = JSON.parse(localStorage.getItem("products")) || []; // Retrieve existing products from localStorage or create an empty array
+  products.push(product); // Add the new product to the products array
+  localStorage.setItem("products", JSON.stringify(products)); // Store the updated products array in localStorage
 
   // Send the product data to the REST API
   fetch("https://crudcrud.com/api/f4aaa73b861842879ad5d520517d03aa", {
-    method: "POST",
+    method: "POST", // HTTP POST request
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json", // Set the request content type to JSON
     },
-    body: JSON.stringify(product)
+    body: JSON.stringify(product), // Send the product object as JSON in the request body
   })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
+    .then((response) => response.json()) // Parse the response JSON
+    .then((data) => console.log(data)) // Log the response data to the console
+    .catch((error) => console.log(error)); // Log any errors that occur
 
   // Clear the input fields
-  document.getElementById("sellingPrice").value = "";
-  document.getElementById("productName").value = "";
+  document.getElementById("sellingPrice").value = ""; // Clear the selling price input field
+  document.getElementById("productName").value = ""; // Clear the product name input field
 
   // Update the product list
-  updateProductList();
+  updateProductList(); // Call the function to update the product list on the page
 }
 
 // Function to delete a product
@@ -47,7 +47,7 @@ function deleteProduct(index) {
   localStorage.setItem("products", JSON.stringify(products));
 
   // Update the product list
-  updateProductList();
+  updateProductList(); // Call the function to update the product list on the page
 }
 
 // Function to edit a product
@@ -56,22 +56,22 @@ function editProduct(index) {
   var products = JSON.parse(localStorage.getItem("products")) || [];
 
   // Get the updated values from the input fields
-  var sellingPrice = document.getElementById("sellingPrice").value;
-  var productName = document.getElementById("productName").value;
+  var sellingPrice = document.getElementById("sellingPrice").value; // Get the updated selling price from the input field
+  var productName = document.getElementById("productName").value; // Get the updated product name from the input field
 
   // Update the product at the specified index
-  products[index].sellingPrice = sellingPrice;
-  products[index].productName = productName;
+  products[index].sellingPrice = sellingPrice; // Update the selling price of the product at the specified index
+  products[index].productName = productName; // Update the product name of the product at the specified index
 
   // Update the products in localStorage
   localStorage.setItem("products", JSON.stringify(products));
 
   // Clear the input fields
-  document.getElementById("sellingPrice").value = "";
-  document.getElementById("productName").value = "";
+  document.getElementById("sellingPrice").value = ""; // Clear the selling price input field
+  document.getElementById("productName").value = ""; // Clear the product name input field
 
   // Update the product list
-  updateProductList();
+  updateProductList(); // Call the function to update the product list on the page
 }
 
 // Function to update the product list
@@ -87,7 +87,7 @@ function updateProductList() {
   var totalValue = 0;
 
   // Iterate over the products and create list items
-  products.forEach(function(product, index) {
+  products.forEach(function (product, index) {
     // Create a list item
     var listItem = document.createElement("li");
     listItem.className = "list-group-item";
@@ -106,12 +106,12 @@ function updateProductList() {
     productList.appendChild(listItem);
 
     // Update the total value
-    totalValue += parseInt(product.sellingPrice);
+    totalValue += parseInt(product.sellingPrice); // Add the selling price to the total value
   });
 
   // Update the total value on the page
-  document.getElementById("totalValue").textContent = totalValue;
+  document.getElementById("totalValue").textContent = totalValue; // Display the total value on the page
 }
 
 // Load the product list on page load
-updateProductList();
+updateProductList(); // Call the function to update the product list when the page loads
